895daee33e2ee366a960b59bbd9b53a70f4abc14
Taarini1 <98458225+Taarini1@users.noreply.github.com>
2023-03-20 17:12:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function Start(){
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier=ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/M4tjnCXV3/model.json",modelLoaded);
+}
+
+function modelLoaded(){
+    console.log("modelLoaded")
+    classifier.classify(gotResult);
+}
+
+function gotResult(error,results){
+    if(error){
+        console.log(error);
+    }
+
+    else{
+        console.log(results);
+    }
+}
